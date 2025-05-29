e3acc77bdfec1edf1a713f0e906fda241c28a69e
Garik <44021019+Garo1995@users.noreply.github.com>
2025-05-29 21:53:04 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+    $('.open-menu').on('click', function () {
+        $(this).toggleClass('close-menu');
+        if ($(this).hasClass('close-menu')) {
+            $('.menu-content').addClass('transition-menu');
+            $('body').addClass('body_fix');
+        } else {
+            $('.menu-content').addClass('menu-width');
+            $('body').removeClass('body_fix');
+            $('.menu-content').removeClass('transition-menu');
+        }
+    });
+    $('.nav-menu a').on('click', function () {
+        $('.menu-content').addClass('menu-width');
+        $('body').removeClass('body_fix');
+        $('.menu-content').removeClass('transition-menu');
+        $('.open-menu').removeClass('close-menu');
+    })
+});
+
+
+
+
+
+$(function () {
+    let Accordion = function (el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+        let links = this.el.find('.link');
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    };
+
+    Accordion.prototype.dropdown = function (e) {
+        let $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+        $next.slideToggle();
+        if (!e.data.multiple) {
+
+            $el.find('.submenu').not($next).slideUp();
+        }
+        if (!$this.hasClass('open')) {
+            $('.link').each(function () {
+                $(this).removeClass('open')
+            })
+            $this.addClass('open')
+        } else {
+            $this.removeClass('open')
+        }
+    }
+    let accordion = new Accordion($('#accordion'), false);
+});
+
+$('.menu-scroll a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+        let $target = $(this.hash);
+        $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+            let targetOffset = $target.offset().top-130;
+            $('html,body')
+                .animate({scrollTop: targetOffset}, 1000);
+            return false;
+        }
+    }
+});
+
+
+
+let productSwiper = new Swiper(".product-cards-slider", {
+    slidesPerView: 6,
+    spaceBetween: 24,
+    loop: true,
+    speed: 1200,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        '1500': {
+            slidesPerView: 6,
+            spaceBetween: 24,
+        },
+        '1200': {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+        '1020': {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        '767': {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        '575': {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        '320': {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 20,
+        },
+    }
+
+});
+
+
+
+
+
+
+
+let trustedSwiper = new Swiper(".trusted-slider", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        '1020': {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        '767': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        '320': {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 8,
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+});
